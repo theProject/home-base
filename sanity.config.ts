0e1f8574626f93c2593454b9f881610329fb4903
theProject - Tristan Smith <tjsmith@bytheproject.com>
2025-04-23 22:06:19 -0400

    test

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,10 +1,27 @@
-import { defineConfig } from "sanity"
+'use client'
+
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
+ */
+
+
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './sanity/env'
+import {schema} from './sanity/schemaTypes'
+import {structure} from './sanity/structure'
 
 export default defineConfig({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  apiVersion: "2023-05-03",
-  title: "theProject Content",
-  basePath: "/studio",
-  plugins: [],
+  basePath: '/studio',
+  projectId,
+  dataset,
+  // Add and edit the content schema in the './sanity/schemaTypes' folder
+  schema,
+  plugins: [
+    structureTool({structure}),
+    // Vision is for querying with GROQ from inside the Studio
+
+  ],
 })
