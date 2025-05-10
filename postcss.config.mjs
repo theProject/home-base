52702811afb54a552554862ebea950c268fbcf29
theProject - Tristan Smith <tjsmith@bytheproject.com>
2025-05-10 14:59:38 -0400

    tailwind 4 fixing

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,9 @@
+// postcss.config.mjs
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, 
+    // just in cae TailwindCSS v.4 still needs it  'autoprefixer': {},
   },
 };
 
