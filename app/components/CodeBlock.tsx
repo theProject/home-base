c9d4dfdcbc885beeb2b9edd36acbfa60aaf96534
Tristan Smith <tjsmith@bytheproject.com>
2025-06-22 19:44:16 -0400

    feat: add syntax highlighting

--- app/components/CodeBlock.tsx
+++ app/components/CodeBlock.tsx
@@ -1,15 +1,36 @@
 // components/CodeBlock.tsx
-import React from 'react'
+'use client'
+
+import React, { useEffect, useRef } from 'react'
+import Prism from 'prismjs'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-typescript'
+import 'prismjs/components/prism-tsx'
+import 'prismjs/components/prism-jsx'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-json'
 
 type CodeProps = {
   language?: string
   code?: string
 }
 
-export default function CodeBlock({ language, code }: CodeProps) {
+export default function CodeBlock({ language = 'javascript', code = '' }: CodeProps) {
+  const codeRef = useRef<HTMLElement>(null)
+
+  useEffect(() => {
+    if (codeRef.current) {
+      Prism.highlightElement(codeRef.current)
+    }
+  }, [code, language])
+
+  const className = language ? `language-${language}` : undefined
+
   return (
     <pre className="code-block">
-      <code data-lang={language}>{code}</code>
+      <code ref={codeRef} className={className} data-lang={language}>
+        {code}
+      </code>
     </pre>
   )
 }
